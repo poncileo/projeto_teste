f2b2bd1a43727535087efcad0738e0a6ec78b760
poncileo <leonardo@bellets.com>
2017-08-14 18:14:15 -0300

    onScroll change navigation bar added

--- js/script.js
+++ js/script.js
@@ -10,15 +10,17 @@ $(document).ready(function() {
         return painelExibido;
     }
     
-    function toggleIdPainel() {
-        var painel = $(".js-popup-painel")[0];
+    function toggleIdPainel(target) {
         
+        var painel = $(".js-popup-painel")[0];
+        var idBotao =  "botao-mobile-menu" ;
+
         var painelAtivo = "box-menu-mobile-ativo";
         var painelInativo = "box-menu-mobile";
 
         if( painel.id == painelAtivo ) {
             painel.id = painelInativo;
-        } else {
+        } else if( painel.id == painelInativo && target.id == idBotao ) {
             painel.id = painelAtivo;
         }
     }
@@ -27,15 +29,37 @@ $(document).ready(function() {
         $(document).click(function(e){
             var target = e.target;
             
-            if( isPainelExibido(target) ) {
-                toggleIdPainel();
+            if( !isPainelExibido(target) ) {
+                toggleIdPainel(target);
             } else {
-                toggleIdPainel();
+                toggleIdPainel(target);
+            }
+        });
+    }
+    
+    function mudarCabecalhoOnScroll() {
+        $(document).scroll(function() {
+            var scroll_start = 0;
+            var start_change = $("#cabecalho");
+            var offset = start_change.offset();
+            var scrollClass = "nav-scroll";
+            
+            if(start_change.length) {
+                scroll_start = $(this).scrollTop();
+                
+                if( scroll_start == 0 ) {
+                    $("header").removeClass(scrollClass);
+                    
+                } else {
+                    $("header").addClass(scrollClass);
+                }
             }
         });
     }
     
     if( tamanhoTela.indexOf("mobile") != -1 ) {
         acaoBotaoMenuMobile();
+    } else if( tamanhoTela.indexOf("desktop") != -1 )  {
+        mudarCabecalhoOnScroll();
     }
 });
